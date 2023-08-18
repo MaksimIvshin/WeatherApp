8e7c1510e36f3d3f0166a8d70f7f3d12d556f078
MaksimIvshin <mi375293086861@gmail.com>
2023-08-18 19:57:34 +0300

    WeatherDataManger

---
+++ WeatherApp/WeatherDataManager.swift
@@ -0,0 +1,29 @@
+//
+//  WeatherDataManager.swift
+//  WeatherApp
+//
+//  Created by Maks Ivshin on 18.08.23.
+//
+
+import Foundation
+import CoreLocation
+
+class WeatherDataManager {
+    let apiKey = "b5a50c398a579b44871a884663a2845a"
+
+
+    func fetchData(forLocation location: CLLocation) {
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.coordinate.latitude)&lon=\(location.coordinate.longitude)&appid=\(apiKey)&units=metric"
+        guard let url = URL(string: urlString) else {return}
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil, let data = data else { return }
+            if let weather = try? JSONDecoder().decode(WeatherData.self, from: data) {
+                DispatchQueue.main.async {
+                    print(weather)
+                }
+            } else {
+                print(String(describing: error?.localizedDescription))
+            }
+        }.resume()
+    }
+}
