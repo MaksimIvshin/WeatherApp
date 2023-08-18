d23c70400d04fd1117416684e9b5fdc7d7b1d7ff
MaksimIvshin <mi375293086861@gmail.com>
2023-08-18 16:24:06 +0300

    WeatherApp on SwiftUI

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Maks Ivshin on 18.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
